bb5528953fc7a01058de4da3241bee68afb94661
Lix <zinebbendjafer9@gmail.com>
2023-12-23 11:00:34 +0100

    fixed responsiveness

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,4 +1,4 @@
-import { BsCart2 } from "react-icons/bs";
+import { BsCart2, BsSearch } from "react-icons/bs";
 import Hamburger from "hamburger-react";
 import "./header.css";
 import { useState } from "react";
@@ -13,20 +13,39 @@ const Header = () => {
         }}
       >
         {!open && (
-          <div className="cart">
+          <div className="cart mobile">
             <BsCart2 />
           </div>
         )}
         <span
+          // className="mobile"
           style={{
             color: open ? "white" : "black",
             fontSize: open ? "2rem" : "1.5rem",
             paddingLeft: open ? "1rem" : ".5rem",
-            paddingTop: open ? "1rem" : ".5rem"
+            paddingTop: open ? "1rem" : ".5rem",
           }}
         >
           ShopIn.
         </span>
+
+        <nav className="desktop">
+          <a href="">About us</a>
+          <a href="">Our products</a>
+          <a href="">New items</a>
+          <a href="">Contact</a>
+        </nav>
+
+        <div className="desktop">
+          <div>
+            <BsSearch />
+            <input type="text" name="" id="" placeholder="Search" />
+          </div>
+          <div className="cart">
+            <BsCart2 />
+          </div>
+        </div>
+
         <button className="hamburger">
           <Hamburger
             size={20}
@@ -42,7 +61,10 @@ const Header = () => {
         </button>
       </div>
 
-      <nav style={{ transform: open ? "translateY(0)" : "translateY(100%)" }}>
+      <nav
+        className="mobile"
+        style={{ transform: open ? "translateY(0)" : "translateY(100%)" }}
+      >
         <a href="">About us</a>
         <a href="">Our products</a>
         <a href="">New items</a>
@@ -50,18 +72,28 @@ const Header = () => {
       </nav>
 
       <div>
-        <div>
-          <h1>Each Piece is Designed to Move with You</h1>
+        <div className="desktop">
+          <img className="desktop" src="/images/model.jpg" alt="" />
           <div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum fermentum risus ac neque feugiat, vel tincidunt nulla
-              cursus. Quisque at elit et urna bibendum posuere. Vivamus euismod.
-            </p>
-            <button>Shop Now</button>
+            <span>Shop Now</span>
           </div>
         </div>
-        <img src="/images/model.jpg" alt="" />
+
+        <div>
+          <div>
+            <h1>Each Piece is Designed to Move with You</h1>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum fermentum risus ac neque feugiat, vel tincidunt nulla
+                cursus. Quisque at elit et urna bibendum posuere. Vivamus
+                euismod.
+              </p>
+              <button className="mobile">Shop Now</button>
+            </div>
+          </div>
+          <img src="/images/model.jpg" alt="" />
+        </div>
       </div>
     </header>
   );
