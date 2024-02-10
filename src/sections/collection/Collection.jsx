6b6ac80d3a7e1917748034ac7a9eeebaab5f8915
Lix <zinebbendjafer9@gmail.com>
2024-02-10 10:52:38 +0100

    collection section

--- src/sections/collection/Collection.jsx
+++ src/sections/collection/Collection.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import "./collection.css";
+import { motion } from "framer-motion";
 const Collection = () => {
   return (
-    <div>Collection</div>
-  )
-}
+    <div className="collection">
+      <div>
+        <motion.h2
+          initial={{ y: 50 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 1, ease: "linear" }}
+        >
+          our new collection is here!
+        </motion.h2>
+      </div>
+      <div>
+        <motion.img
+          initial={{ y: 50 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 1, ease: "linear" }}
+          src="/images/model.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Collection
+export default Collection;
