ff97b175a8bd6d73384f4f6f2ec7adc4e4cc90f1
Lix <zinebbendjafer9@gmail.com>
2024-03-11 15:24:37 +0100

    optimizartion

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'semistandard'
+  ],
+  plugins: [
+    'unused-imports'
+  ],
+  rules: {
+    // Add or update rules as needed for your project
+    'unused-imports/no-unused-imports': 'error'
+    // Other rules...
+  },
+};
--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,15 +1,14 @@
-// eslint-disable-next-line react/prop-types
 const ImageSlider = ({ slider }) => {
   return (
     <div ref={slider} className="slider">
-      <img src="/images/col1.webp" alt="" className="panel desktop" />
-      <img src="/images/col2.webp" alt="" className="panel desktop" />
-      <img src="/images/col3.webp" alt="" className="panel desktop" />
-      <img src="/images/col4.webp" alt="" className="panel desktop" />
-      <img src="/images/mobile/col1.webp" alt="" className="panel mobile" />
-      <img src="/images/mobile/col2.webp" alt="" className="panel mobile" />
-      <img src="/images/mobile/col3.webp" alt="" className="panel mobile" />
-      <img src="/images/mobile/col4.webp" alt="" className="panel mobile" />
+      <img loading="lazy" src="/images/col1.webp" alt="" className="panel desktop" />
+      <img loading="lazy" src="/images/col2.webp" alt="" className="panel desktop" />
+      <img loading="lazy" src="/images/col3.webp" alt="" className="panel desktop" />
+      <img loading="lazy" src="/images/col4.webp" alt="" className="panel desktop" />
+      <img loading="lazy" src="/images/mobile/col1.webp" alt="" className="panel mobile" />
+      <img loading="lazy" src="/images/mobile/col2.webp" alt="" className="panel mobile" />
+      <img loading="lazy" src="/images/mobile/col3.webp" alt="" className="panel mobile" />
+      <img loading="lazy" src="/images/mobile/col4.webp" alt="" className="panel mobile" />
     </div>
   );
 };
